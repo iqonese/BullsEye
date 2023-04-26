cffeb58c757d3a6501fe3e53737ca57df318ae40
iqonese <iqonese@gmail.com>
2023-04-26 20:28:40 +0600

    Project Finished

--- bullseye/ContentView.swift
+++ bullseye/ContentView.swift
@@ -1,88 +1,52 @@
-//
-//  ContentView.swift
-//  bullseye
-//
-//  Created by Said Tura Saidazimov on 26.04.2023.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var target = Int.random(in: 1...99)
+    @State var userSelection = 50.0
+    @State var alertIsVisible = false
+//    @State var round = 1
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        VStack {
+            Spacer()
+            HStack {
+                Text("Put the bullseye as close to the target as you can: ")
+                Text("\(target)")
+            }  // target
+            Spacer()
+            HStack{
+                Text("1")
+                Slider(value: $userSelection,in: 1...100)
+                    .accentColor(.green)
+                Text("100")
+            }.padding()  // slider
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action:{alertIsVisible = true}) {
+                    Text("Hit me!")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+                Spacer()
+            } // confirm button
+            Spacer()
+        }
+        .alert(isPresented: $alertIsVisible) { () -> Alert in
+            let roundedValue = Int(self.userSelection.rounded())
+            let accuracy = 100 - abs(target - roundedValue)
+            return Alert(title: Text("Game Over!"),
+                         message: Text("You chose: \(roundedValue). \n" +
+                                      "The target was: \(target). \n" +
+                                       "Your accuracy: \(accuracy)%"), dismissButton: .default(Text("Awesome!")) {
+//                self.round += 1
+                self.target = Int.random(in: 1...99)
+                self.userSelection = 50.0
+            })
+            
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
